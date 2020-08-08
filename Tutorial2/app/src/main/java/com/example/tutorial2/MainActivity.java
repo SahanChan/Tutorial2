b0fa0120e2d98b87656137824a44f35390db4b77
sahan <xnscb8.chandrabahu@gmail.com>
2020-08-08 19:53:26 +0530

    functionsInvoking

--- Tutorial2/app/src/main/java/com/example/tutorial2/MainActivity.java
+++ Tutorial2/app/src/main/java/com/example/tutorial2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tutorial2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,43 @@ public class MainActivity extends AppCompatActivity {
 
         textView2 = findViewById(R.id.textView2);
         textView2.setText(R.string.Msg2);
+        Log.i("Lifecycle","OnCreate() invoked");
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Lifecycle","OnStart() invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Lifecycle","OnRestart() invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Lifecycle","OnResume() invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Lifecycle","OnPause() invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Lifecycle","OnStop() invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Lifecycle","OnDestroy() invoked");
     }
 }
